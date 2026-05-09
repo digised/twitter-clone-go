c7d41b817e091c61e98ee3a9970366174a22f379
Amin <amin.noorbakhsh83@gmail.com>
2026-05-09 12:58:55 +0330

    refactor: update project structure

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,19 +1,1 @@
 package main
-
-import (
-	"log"
-	"twitter-clone/internal/routes"
-
-	"github.com/gin-gonic/gin"
-)
-
-func main() {
-	router := gin.Default()
-
-	routes.RegisterRoutes(router)
-
-	err := router.Run(":8080")
-	if err != nil {
-		log.Fatal("Failed to start server:", err)
-	}
-}
--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -1,11 +1,1 @@
 package routes
-
-import (
-	"twitter-clone/internal/handlers"
-
-	"github.com/gin-gonic/gin"
-)
-
-func RegisterRoutes(r *gin.Engine) {
-	r.GET("/ping", handlers.PingHandler)
-}
