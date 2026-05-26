03b1b27fb954d59242b744b923a9214712369583
Amin <amin.noorbakhsh83@gmail.com>
2026-05-26 13:51:45 +0330

    chore: update user

--- internal/user/cache.go
+++ internal/user/cache.go
@@ -1,1 +1,36 @@
 package user
+
+import (
+	"context"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Cache interface {
+	SetUser(
+		ctx context.Context,
+		user *User,
+		ttl time.Duration,
+	) error
+
+	GetUserByID(
+		ctx context.Context,
+		id uuid.UUID,
+	) (*User, error)
+
+	GetUserByUsername(
+		ctx context.Context,
+		username string,
+	) (*User, error)
+
+	DeleteUserByID(
+		ctx context.Context,
+		id uuid.UUID,
+	) error
+
+	DeleteUserByUsername(
+		ctx context.Context,
+		username string,
+	) error
+}
--- internal/user/repository.go
+++ internal/user/repository.go
@@ -1,1 +1,87 @@
-﻿package user
+package user
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+)
+
+type Repository interface {
+	Create(
+		ctx context.Context,
+		user *User,
+	) error
+
+	GetByID(
+		ctx context.Context,
+		id uuid.UUID,
+	) (*User, error)
+
+	GetByUsername(
+		ctx context.Context,
+		username string,
+	) (*User, error)
+
+	GetByEmail(
+		ctx context.Context,
+		email string,
+	) (*User, error)
+
+	UsernameExists(
+		ctx context.Context,
+		username string,
+	) (bool, error)
+
+	EmailExists(
+		ctx context.Context,
+		email string,
+	) (bool, error)
+
+	Update(
+		ctx context.Context,
+		user *User,
+	) error
+
+	UpdateProfile(
+		ctx context.Context,
+		id uuid.UUID,
+		displayName *string,
+		bio *string,
+		avatarURL *string,
+	) error
+
+	Delete(
+		ctx context.Context,
+		id uuid.UUID,
+	) error
+
+	IncrementFollowersCount(
+		ctx context.Context,
+		id uuid.UUID,
+	) error
+
+	DecrementFollowersCount(
+		ctx context.Context,
+		id uuid.UUID,
+	) error
+
+	IncrementFollowingCount(
+		ctx context.Context,
+		id uuid.UUID,
+	) error
+
+	DecrementFollowingCount(
+		ctx context.Context,
+		id uuid.UUID,
+	) error
+
+	IncrementTweetsCount(
+		ctx context.Context,
+		id uuid.UUID,
+	) error
+
+	DecrementTweetsCount(
+		ctx context.Context,
+		id uuid.UUID,
+	) error
+}
